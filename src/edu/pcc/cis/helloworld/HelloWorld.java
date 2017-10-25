e3465ac9ad41b36473a7d92fbeec8b3fc100918b
sushil.aryal <sushil.aryal@pcc.edu>
2017-10-24 18:16:08 -0700

    Sushil Aryal added line to print date after adding import java.util.Date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /*
  * Created By Sushil Aryal on 2016/10/24
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
