f73773a556e53615f08c2712d4b159ec78602333
sushil.aryal <sushil.aryal@pcc.edu>
2017-10-24 18:04:11 -0700

    Sushil Aryal added main method to print Hello World

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/*
+ * Created By Sushil Aryal on 2016/10/24
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
